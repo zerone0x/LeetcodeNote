bf3667ac2d049e25f8291b6541d611f214362ccf
dot <soberzml2333@gmail.com>
2022-03-16 18:34:05 +0800

    update

---
+++ python/interval/merge.py
@@ -0,0 +1,23 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # intervals.sort(key = lambda i: i[0])
+        # # sort的key 通过lambda 设置条件来排序
+        # out = [intervals[0]]
+        # # out初始化 --> 第一个interval
+        # for start, end in intervals[1:]:
+        #     if out[-1][1] < start:
+        #         out.append(start, end)
+        #     else:
+        #         out[-1, 1] = max(out[-1][1], end)
+        # return out
+    
+    # 条件是比较第一个序列的尾和下一个的头
+    #med
+    #整体算简单。用到了sort 的 key
+    out = []
+    for i in sorted(intervals, key = lambda i: i.start):
+        if not out or out[-1][1] < i[0]:
+            out.append(i)
+        else:
+            out[-1].end = max(out[-1][1], i[1])
+    return out
