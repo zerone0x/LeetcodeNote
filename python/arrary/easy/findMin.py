14d45d4965e68927ab24a42554b32192721dfef7
dot <soberzml2333@gmail.com>
2022-03-19 00:51:42 +0800

    update

---
+++ python/arrary/easy/findMin.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] <= nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m+ 1
+            else:
+                r = m-1
+        return res
+        # l, r = 0, len(nums) - 1
+        # while l < r:
+        #     m = (l + r) // 2
+        #     if nums[m] > nums[r]:
+        #         l = m+1
+        #     else:
+        #         r = m
+        # return nums[l]
