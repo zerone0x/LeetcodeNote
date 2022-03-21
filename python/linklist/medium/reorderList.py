3e81e9d8290d5e8a70cb4430e790bd8019fca35b
dot <soberzml2333@gmail.com>
2022-03-21 21:51:23 +0800

    update

--- python/linklist/medium/reorderList.py
+++ python/linklist/medium/reorderList.py
@@ -8,4 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        slow, fast = head, head.next
+        while fast and fast.next:
+                        # fast.next 条件容易忽略
+            slow, fast = slow.next, fast.next.next
         
+        second = slow.next
+        prev = slow.next = None
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        # reverse list
+        
+        first = head
+        second = prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            second.next = tmp1
+            first.next = second
+            first, second = tmp1, tmp2
