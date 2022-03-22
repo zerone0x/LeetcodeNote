71229c6ef83dcb79fa3061bbed68df6fb6cca80a
dot <soberzml2333@gmail.com>
2022-03-22 11:40:09 +0800

    update

--- python/string/med/lengthOfLongestSubstring.py
+++ python/string/med/lengthOfLongestSubstring.py
@@ -1,3 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+        charset = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in charset:
+                charset.remove(s[l])
+                l += 1
+            charset.add(s[r])
+            # 遍历s，如果有相同的值，remove左指针，并且左指针向右。
+            # 如果没有相同的值，那就添加到s里
+            
+            res = max(res, r-l+1)
+        return res
