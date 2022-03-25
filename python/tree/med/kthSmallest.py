259d8ac6e5e92d6890e58bb3c655da4ad6853d1e
dot <soberzml2333@gmail.com>
2022-03-25 21:26:12 +0800

    update

--- python/tree/med/kthSmallest.py
+++ python/tree/med/kthSmallest.py
@@ -4,22 +4,40 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from cgitb import reset
+
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        stack = []
-        curr = root
-        n = 0
+        # iterative
+        # n = 0
+        # stack = []
+        # cur = root
         
-        while curr and stack:
-            while curr:
-                stack.append(curr)
-                # 将node放在stack中
-                curr = curr.left
+        # while cur or stack:
+        #     while cur:
+        #         stack.append(cur)
+        #         # 将node放在stack中
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     n += 1
+        #     if n == k:
+        #         return cur.val
+        #     # 返回node的value
+        #     cur = cur.right
                 
-            curr = stack.pop()
-            n += 1
-            if n == k:
-                return curr.val
-            # 返回node的value
-            curr = curr.right
-                
+        # recursive
+        self.k = k 
+        self.res = None
+        self.help(root)
+        return self.res
+        
+        def help(self, node):
+            if not node:
+                return 
+            self.help(node.left)
+            self.k -= 1
+            if self.k == 0:
+                self.res = node.val
+                return 
+            self.help(node.right)
