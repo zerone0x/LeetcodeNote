2aa65e9b228a5edba8069b1c77621ba65690d75a
dot <soberzml2333@gmail.com>
2022-03-26 22:35:43 +0800

    update

---
+++ python/tree/med/levelOrder.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import queue
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = collections.deque()
+        res = []
+        q.append(root)
+        
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+                
+        数据结构 queue
+        结果 list
+        输入 root
+        遍历队列 队列分层结果list，输出第一个节点，放到level里。
+        
