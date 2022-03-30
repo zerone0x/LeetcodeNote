fce7d5a5115ffdea59c468337677bfce0bb83e41
dot <soberzml2333@gmail.com>
2022-03-30 23:42:45 +0800

    magic for fun

---
+++ python/dp/med/rob.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # res = 0
+        # n = len(nums)
+        # for i in range(nums):
+        #     res = max(res, nums[i] + self.rob(nums[i+1 : n]))
+        # return res
+        rob1, rob2 = 0, 0
+        for n in nums:
+            tmp = max(rob1+n, rob2)
+            rob1 = rob2
+            rob2 = tmp
+            
+        return rob2
