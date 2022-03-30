28e79235306345ffe8d810bc448bcae31aa479c5
dot <soberzml2333@gmail.com>
2022-03-31 00:40:49 +0800

    magic for fun

---
+++ python/dp/med/coinChange.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # if amount > max(coins):
+        #     return -1
+        # if amount == 0:
+        #     return 0
+        dp = (amount+1)*[amount + 1]
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for j in coins:
+                if i-j >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-j])
+                
+        return dp[amount] if dp[amount] != amount+1 else -1
+        
+        # 遍历amount从0到最大值，每个值分解为所有的coin，如果不是-1，那就每次取它记录，遍历所有得到最小值。
--- python/dp/med/rob.py
+++ python/dp/med/rob.py
@@ -7,8 +7,11 @@ class Solution:
         # return res
         rob1, rob2 = 0, 0
         for n in nums:
-            tmp = max(rob1+n, rob2)
-            rob1 = rob2
-            rob2 = tmp
-            
+            # tmp = max(rob1+n, rob2)
+            # rob1 = rob2
+            # rob2 = tmp
+            rob1, rob2 = rob2,max(rob1+n, rob2)
         return rob2
+# 比较当前index的rob值和前1个的rob值大小。
+# 当前值是n+rob1
+# 前一个值是rob2
