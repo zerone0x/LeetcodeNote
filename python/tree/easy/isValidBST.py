776c7ee2444f324d63e954e129f3ba44b28f428f
dot <soberzml2333@gmail.com>
2022-04-01 00:07:08 +0800

    magic for fun

--- python/tree/easy/isValidBST.py
+++ python/tree/easy/isValidBST.py
@@ -4,6 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def vaild(node, left, right):
+            if not node:
+                return True
+            if not (node.val > left and node.val < right):
+                return False    
+            return (vaild(node.left, left, node.val) and vaild(node.right, node.val, right))
+        return vaild(root, float("-inf"), float("inf"))
         
